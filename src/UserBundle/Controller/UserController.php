55a8e8bc00399ef4ef9c4cf3c1a3df82a08f3058
Jose Gomez <josegomez120@gmail.com>
2017-05-16 23:37:09 -0500

    FEAT: Agregado Rutas CRUD USER

---
+++ src/UserBundle/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+        return new Response('Hola newbie');
+    }
+}
