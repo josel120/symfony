d5ff68541d292f30f822c10d89b02dd7606a4a10
Jose Gomez <josegomez120@gmail.com>
2017-05-20 21:15:50 -0500

    rutas y controlador crud

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+    * @Route("/", name="homepage")
+    *
+    */
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace UserBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,5 +11,21 @@ class UserController extends Controller
     public function indexAction()
     {
         return new Response('Hola newbie');
+    }
+    public function addAction()
+    {
+        return new Response('Hola agregar ');
+    }
+    public function editAction($id)
+    {
+        return new Response('Hola editar '.$id);
+    }
+    public function viewAction($id)
+    {
+        return new Response('Hola detalles '.$id);
+    }
+    public function deleteAction($id)
+    {
+        return new Response('Hola eliminar  '.$id);
     }
 }
